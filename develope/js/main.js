87c925f20e13d94624b2ce3026e4a2dc179273e7
krzysiek <krzysztof.kacik@sarigato.com>
2016-11-21 11:50:15 +0100

    stable version

---
+++ develope/js/main.js
@@ -0,0 +1,17 @@
+var imie = 'john';
+function function_name (obj) {
+	return obj;
+}
+function_name(imie);
+// BS JS ADD '*' IN FRONT OF '= require' to add js file
+//= require ../../node_modules/bootstrap/js/dist/alert.js
+//= require ../../node_modules/bootstrap/js/dist/button.js
+//= require ../../node_modules/bootstrap/js/dist/carousel.js
+//= require ../../node_modules/bootstrap/js/dist/collapse.js
+//= require ../../node_modules/bootstrap/js/dist/dropdown.js
+//= require ../../node_modules/bootstrap/js/dist/modal.js
+//= require ../../node_modules/bootstrap/js/dist/popover.js
+//= require ../../node_modules/bootstrap/js/dist/scrollspy.js
+//= require ../../node_modules/bootstrap/js/dist/tab.js
+//= require ../../node_modules/bootstrap/js/dist/tooltip.js
+//= require ../../node_modules/bootstrap/js/dist/util.js
